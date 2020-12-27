126718637786751102166f0defbb06a4f66cc7f8
Puraskrit <puraskritsinha3@gmail.com>
2020-12-27 22:51:52 +0530

    a

--- src/com/puraskrit/Main.java
+++ src/com/puraskrit/Main.java
@@ -1,8 +1,28 @@
 package com.puraskrit;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+        System.out.println(fibonacci(n));
     }
+        public static int fibonacci(int n) {
+
+            if(n==0){
+                return 0;
+            }
+            if(n==1){
+                return 1;
+            }
+            int sum=fibonacci(n-1)+fibonacci(n-2);
+            return sum;
+            // Complete the function.
+        }
+
+
 }
