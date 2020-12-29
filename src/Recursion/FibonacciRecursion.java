3b1fe9b66cbc839c5300d4807b463ad8ad4e722d
Puraskrit <puraskritsinha3@gmail.com>
2020-12-29 22:16:32 +0530

    recursion

--- src/Recursion/FibonacciRecursion.java
+++ src/Recursion/FibonacciRecursion.java
@@ -1,8 +1,8 @@
-package com.puraskrit;
+package Recursion;
 
 import java.util.Scanner;
 
-public class Main {
+public class FibonacciRecursion {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
